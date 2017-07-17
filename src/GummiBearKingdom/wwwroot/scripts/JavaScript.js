d5d2bd04c8e820f627bd0146c55c693a2438c6a5
Elise St Hilaire <eliseylenore@gmail.com>
2017-07-17 16:47:06 -0700

    Add selected class to secondary nav items

---
+++ src/GummiBearKingdom/wwwroot/scripts/JavaScript.js
@@ -0,0 +1,9 @@
+﻿/*$(document).ready(function () {
+    console.log('What up bros')
+    $('button').on('click', function () {
+        console.log('heyyy!')
+        $(this).siblings().removeClass('selected')
+        $(this).addClass('selected');
+    })
+})*/
+
